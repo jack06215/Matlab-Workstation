07565d91b7679fa5e7bdb5b4085bb48219f8a867
Jack Cho <jack06215@gmail.com>
2015-12-07 15:56:05 +1100

    vanishing point estimation and warping sample code

--- VanishingPoint_working/demo_final.m
+++ VanishingPoint_working/demo_final.m
@@ -1,15 +1,25 @@
 %% Read image and convert it to grayscale
-refImg = imread('DSC_0764.jpg');
-currentImg = imread('DSC_0765.JPG');
+refImg = imread('BuildingA_original.png');
+currentImg = imread('BuildingA_sheer+rotate.png');
 refImg_gray = rgb2gray(refImg);
 currentImg_gray = rgb2gray(currentImg);
-
 %% Vanishing Point Detection
 [refImg_lineSegment, refImg_vanishing, refImg_points, refImg_foc] = detectTOVP(refImg_gray);
 [currentImg_lineSegment, currentImg_vanishing, currentImg_points, currentImg_foc] = detectTOVP(currentImg_gray);
 refImg_movingPts = vertcat(refImg_lineSegment(:,[1,3]), refImg_lineSegment(:,[2,4]));
 [ refImg_estimateMovingPts ] = triangleMeasure( refImg_movingPts, refImg_vanishing, currentImg_vanishing  );
+%% Show Vanishing Point
+% figure;
+% imshow(refImg), hold on;
+% plot(refImg_lineSegment(:,1),refImg_lineSegment(:,3),'x', 'Color', 'Yellow', 'LineWidth', 2);
+% plot(refImg_lineSegment(:,2),refImg_lineSegment(:,4),'x', 'Color', 'Green', 'LineWidth', 2);
+% showTOVP( refImg, refImg_gray, refImg_vanishing, refImg_points, refImg_foc);
 
+% figure;
+% imshow(currentImg), hold on;
+% plot(currentImg_lineSegment(:,1), currentImg_lineSegment(:,3),'x', 'Color', 'Yellow', 'LineWidth', 2);
+% plot(currentImg_lineSegment(:,2), currentImg_lineSegment(:,4),'x', 'Color', 'Green', 'LineWidth', 2);
+% showTOVP(currentImg, currentImg_gray, currentImg_vanishing, currentImg_points, currentImg_foc);
 %% Triangulation Measurement
 figure, hold on;
 xlim([-3000,3000]);
@@ -32,29 +42,21 @@ plot([currentImg_vanishing(2,1), currentImg_vanishing(3,1)], [currentImg_vanishi
 % Moving points bounded by vanishing points B
 plot(refImg_estimateMovingPts(:,1), refImg_estimateMovingPts(:,2), '*', 'Color', 'Cyan', 'MarkerSize', 5, 'LineWidth', 1);
 
+% Show displacement trajectory
 for i=1:size(refImg_movingPts,1)
     plot([refImg_movingPts(i,1), refImg_estimateMovingPts(i,1)], [refImg_movingPts(i,2), refImg_estimateMovingPts(i,2)], '-', 'Color', 'Blue', 'MarkerSize', 5, 'LineWidth', 1);
 end
 
 %% Estimate homography besed on moving points
-[tform,inlierPtsDistorted,inlierPtsOriginal] = estimateGeometricTransform(refImg_movingPts, refImg_estimateMovingPts, 'affine');
+[tform,inlierPtsDistorted,inlierPtsOriginal] = estimateGeometricTransform(refImg_movingPts, refImg_estimateMovingPts, 'projective');
 outImg = imwarp(refImg, tform);
 figure;
 imshow(currentImg);
-% figure;
-% imshow(refImg);
+title('Current');
+figure;
+imshow(refImg);
+title('Previous');
 figure;
 imshow(outImg);
+title('Warp');
 
-%% Show Vanishing Point
-% figure;
-% imshow(refImg), hold on;
-% plot(refImg_lineSegment(:,1),refImg_lineSegment(:,3),'x', 'Color', 'Yellow', 'LineWidth', 2);
-% plot(refImg_lineSegment(:,2),refImg_lineSegment(:,4),'x', 'Color', 'Green', 'LineWidth', 2);
-% showTOVP( refImg, refImg_gray, refImg_vanishing, refImg_points, refImg_foc);
-
-% figure;
-% imshow(currentImg), hold on;
-% plot(currentImg_lineSegment(:,1), currentImg_lineSegment(:,3),'x', 'Color', 'Yellow', 'LineWidth', 2);
-% plot(currentImg_lineSegment(:,2), currentImg_lineSegment(:,4),'x', 'Color', 'Green', 'LineWidth', 2);
-% showTOVP(currentImg, currentImg_gray, currentImg_vanishing, currentImg_points, currentImg_foc);
