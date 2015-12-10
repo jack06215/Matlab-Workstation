173640dd3d9aa266ec4d51499ad590b5b5010e53
Jack Cho <jack06215@gmail.com>
2015-12-10 14:53:44 +1100

    Concatenate image grid / strips into one mat

--- (Random Workstation)/imageStripExample.m
+++ (Random Workstation)/imageStripExample.m
@@ -1,12 +1,13 @@
-refImg = imread('Sample Images\fig1.jpg');
+refImg = imread('Sample Images\dsc004901.jpg');
 refImg_BW = rgb2gray(refImg);
-num_vertStrip = 5;
-num_horzStrip = 5;
+num_vertStrip = 4;
+num_horzStrip = 1;
 num_dim = 3;
 
 col_cut = repmat(size(refImg,2)/num_vertStrip,[1 num_vertStrip]);
 row_cut = repmat(size(refImg,1)/num_horzStrip,[1 num_horzStrip]);
-stripImg = mat2cell(refImg, row_cut, col_cut, num_dim);
+stripRefImg = mat2cell(refImg, row_cut, col_cut, num_dim);
+stripGray = cell(size(row_cut,2), size(col_cut,2));
 
 done = false;
 plot_row = 1;
@@ -14,16 +15,20 @@ plot_col = 1;
 plot_index = 1;
 
 while(not(done))
-    % disp(['[', num2str(plot_row), ', ', num2str(plot_col), ']']);
-    subplot(size(stripImg, 1), size(stripImg, 2), plot_index);
-    imshow(stripImg{plot_row,plot_col});
+%     disp(['[', num2str(plot_row), ', ', num2str(plot_col), ']']);
+    stripGray{plot_row, plot_col} = rgb2gray(stripRefImg{plot_row,plot_col});
+    subplot(size(stripRefImg, 1), size(stripRefImg, 2), plot_index);
+    imshow(stripGray{plot_row,plot_col});
     plot_col = plot_col + 1;
-    if (plot_col > size(stripImg,2))
+    if (plot_col > size(stripRefImg,2))
         plot_row = plot_row + 1;
         plot_col = 1;
-        if (plot_row > size(stripImg,1))
+        if (plot_row > size(stripRefImg,1))
             done = true;
         end
     end
     plot_index = plot_index + 1;
 end
+figure;
+refImg_modify = cell2mat(stripGray);
+imshow(refImg_modify);
