ffb9455f7bb4d7ff7145167699a7d8d636d7e462
Jack Cho <jack06215@gmail.com>
2015-12-15 15:37:02 +1100

    backup

--- (Random Workstation)/updateMultipleSubplot.m
+++ (Random Workstation)/updateMultipleSubplot.m
@@ -23,7 +23,7 @@ figure(curFrm_figHandle);       % Switch to curFrm handle
 
 %% Create the sub-windows for each image strip
 for i = 1:num_gridStrip
-    curFrm_figHandle(i) = subplot(size(curFrm_strip, 1),size(curFrm_strip, 2),i);
+    curFrm_subplotHandle(i) = subplot(size(curFrm_strip, 1),size(curFrm_strip, 2),i);
     title(['Moving', num2str(i)]);
 end
 
@@ -36,7 +36,7 @@ plot_index = 1;     % Plot index
 % Loop through the entire curFrm figure handle
 while(not(done))
     % Switch subplot handle, show the image
-    subplot(curFrm_figHandle(plot_index));
+    subplot(curFrm_subplotHandle(plot_index));
     imshow(curFrm_strip{plot_row,plot_col}), truesize;
     plot_col = plot_col + 1;
     % If column reach the end, go to next row
@@ -57,20 +57,34 @@ done = false;       % Boolean indicator
 plot_row = 1;       % Row index
 plot_col = 1;       % Column index
 plot_index = 1;     % Plot index
-
 while(not(done))
     % Switch to subplot(sp1) handle
-    subplot(curFrm_figHandle(plot_index)), hold on;
+    figure(curFrm_figHandle);
+    subplot(curFrm_subplotHandle(plot_index)), hold on;
     title('Select Point HERE', 'Color', 'Red');
     % Wait for user to select points
     [PtRefX,PtRefY] = getpts; 
     groundRef_X1Y1 = [PtRefX(1), PtRefY(1)];
     groundRef_X2Y2 = [PtRefX(2), PtRefY(2)];
+    
+    col_offset = sum(num_col_strips(1:plot_col)) - num_col_strips(1);
+    row_offset = sum(num_row_strips(1:plot_row)) - num_row_strips(1);
+
+    realCoor_X1Y1 = groundRef_X1Y1 + [col_offset, row_offset];
+    realCoorRef_X2Y2 = groundRef_X2Y2 + [col_offset, row_offset];
+    
     plot(groundRef_X1Y1(1),groundRef_X1Y1(2),'o','Color','Yellow', 'LineWidth', 3);
-    plot(groundRef_X2Y2(1),groundRef_X2Y2(2),'o','Color','Yellow', 'LineWidth', 3);
+    plot(groundRef_X2Y2(1),groundRef_X2Y2(2),'o','Color','Cyan', 'LineWidth', 3);
     line(PtRefX,PtRefY, 'Color', 'Blue', 'LineWidth', 3);
-    title(['Image Strip ', num2str(plot_index)], 'Color', 'Black');
+    title(['Strip[', num2str(plot_index), ']'], 'Color', 'Black');
     plot_col = plot_col + 1;
+    
+    figure(refFrm_figHandle);       % Switch to refFrm handle
+    hold on;
+    plot(realCoor_X1Y1(1),realCoor_X1Y1(2),'o','Color','Yellow', 'LineWidth', 3);
+    plot(realCoorRef_X2Y2(1),realCoorRef_X2Y2(2),'o','Color','Cyan', 'LineWidth', 3);
+    subplot(curFrm_subplotHandle(plot_index)), hold on;
+
     % If column reach the end, go to next row
     if (plot_col > size(curFrm_strip,2))
         plot_row = plot_row + 1;
