c6a111444a1e8d3ba020fd2a7535e3369e0d7a81
Jack Cho <jack06215@gmail.com>
2015-12-15 18:32:24 +1100

    backup

---
+++ (Random Workstation)/Untitled.m
@@ -0,0 +1,2 @@
+figure;
+[X,Y] = getpts;
--- (Random Workstation)/updateMultipleSubplot.m
+++ (Random Workstation)/updateMultipleSubplot.m
@@ -1,26 +1,32 @@
 close all; clear;
 %% Read in image
-curImg = imread('DSC_0764.JPG');                    % Curent image
-estImg = imread('DSC_0765.JPG');                    % Estimate image
+refFrm = imread('DSC_0765.JPG');                    % Reference image
+curFrm = imread('DSC_0764.JPG');                    % Curent image
 
 %% Define how to cut an image
-num_vertStrip = 4;                                  % Number of vertical strips
+num_vertStrip = 2;                                  % Number of vertical strips
 num_horzStrip = 1;                                  % Number of horizontal strips
 num_gridStrip = num_vertStrip * num_horzStrip;      % Number of grids in image
 num_chn = 3;                                        % Number of channel in the image 
 
 %% Create parameter for cutting image into strips
-num_col_strips = repmat(size(curImg,2)/num_vertStrip,[1 num_vertStrip]);
-num_row_strips = repmat(size(curImg,1)/num_horzStrip,[1 num_horzStrip]);
-curFrm_strip = mat2cell(curImg, num_row_strips, num_col_strips, num_chn);
+num_col_strips = repmat(size(curFrm,2)/num_vertStrip,[1 num_vertStrip]);
+num_row_strips = repmat(size(curFrm,1)/num_horzStrip,[1 num_horzStrip]);
+curFrm_strip = mat2cell(curFrm, num_row_strips, num_col_strips, num_chn);
 
 %% Construct figure and windows
-curFrm_figHandle = figure;              % Create refFrm figure handle
-figure(curFrm_figHandle);               % Switch to refFrm handle
-imshow(curImg), truesize;               % Display the refFrm
+refFrm_figHandle = figure;              % Create refFrm figure handle
+figure(refFrm_figHandle);               % Switch to refFrm figure handle
+imshow(refFrm), truesize;               % Display the refFrm
+title('Reference Image');               % Title
+
+curFrm_figHandle = figure;              % Create curFrm figure handle
+figure(curFrm_figHandle);               % Switch to curFrm handle
+imshow(curFrm), truesize;               % Display the curFrm
 title('Current Image');                 % Title
-curFrm_strip_figHandle = figure;        % Create curFrm figure handle
-figure(curFrm_strip_figHandle);         % Switch to curFrm handle
+
+curFrm_strip_figHandle = figure;        % Create curFrm strip handle
+figure(curFrm_strip_figHandle);         % Switch to curFrm strip handle
 
 %% Create the sub-windows for each image strip
 for i = 1:num_gridStrip
@@ -58,52 +64,52 @@ end
 done = false;       % Boolean indicator
 plot_row = 1;       % Row index
 plot_col = 1;       % Column index
-plot_index = 0;     % Plot index
+plot_index = 1;     % Plot index
 curFrm_point = [];
 while(not(done))
-    disp(['[', num2str(plot_row), ', ', num2str(plot_col), ']']);
-    % Switch to subplot handle
-    plot_index = plot_index + 1;
+    % disp(['[', num2str(plot_row), ', ', num2str(plot_col), ']']);
     figure(curFrm_strip_figHandle);
     subplot(curFrm_subplotHandle(plot_index)), hold on;
     title('Select Point HERE', 'Color', 'Red');
-    
     % Wait for user to select points
-    [PtRefX,PtRefY] = getpts; 
-    groundRef_X1Y1 = [PtRefX(1), PtRefY(1)];
-    groundRef_X2Y2 = [PtRefX(2), PtRefY(2)];
-    
-    % Offset the image strip's coordinate system to match up with refFrm
-    col_offset = sum(num_col_strips(1:plot_col)) - num_col_strips(1);
-    row_offset = sum(num_row_strips(1:plot_row)) - num_row_strips(1);
-    realCoor_X1Y1 = groundRef_X1Y1 + [col_offset, row_offset];
-    realCoor_X2Y2 = groundRef_X2Y2 + [col_offset, row_offset];
-    
-    % Store the pixel location
-    curFrm_point = [curFrm_point; realCoor_X1Y1]; 
-    curFrm_point = [curFrm_point; realCoor_X2Y2]; 
-    plot(groundRef_X1Y1(1),groundRef_X1Y1(2),'o','Color','Yellow', 'LineWidth', 3);
-    plot(groundRef_X2Y2(1),groundRef_X2Y2(2),'o','Color','Cyan', 'LineWidth', 3);
-    title(['Strip[', num2str(plot_index), ']'], 'Color', 'Black');
-    
-    % Switch to refFrm handle and draw result
-    figure(curFrm_figHandle);       
-    hold on;
-    plot(realCoor_X1Y1(1),realCoor_X1Y1(2),'o','Color','Yellow', 'LineWidth', 3);
-    plot(realCoor_X2Y2(1),realCoor_X2Y2(2),'o','Color','Cyan', 'LineWidth', 3);
+    [PtRefX,PtRefY] = getpts;
+    if (size(PtRefX,1) > 0 & size(PtRefY) > 0)
+        groundRef_X1Y1 = [PtRefX(1), PtRefY(1)];
+        groundRef_X2Y2 = [PtRefX(2), PtRefY(2)];
 
-    % ((((This part handle the image strip indexing))))
-    plot_col = plot_col + 1;
-    % If column reach the end, go to next row
-    if (plot_col > size(curFrm_strip,2))
-        plot_row = plot_row + 1;
-        plot_col = 1;
-        % Done with plotting, exit the loop
-        if (plot_row > size(curFrm_strip,1))
-            done = true;
+        % Offset the image strip's coordinate system to match up with refFrm
+        col_offset = sum(num_col_strips(1:plot_col)) - num_col_strips(1);
+        row_offset = sum(num_row_strips(1:plot_row)) - num_row_strips(1);
+        realCoor_X1Y1 = groundRef_X1Y1 + [col_offset, row_offset];
+        realCoor_X2Y2 = groundRef_X2Y2 + [col_offset, row_offset];
+
+        % Store the pixel location
+        curFrm_point = [curFrm_point; realCoor_X1Y1]; 
+        curFrm_point = [curFrm_point; realCoor_X2Y2]; 
+        plot(groundRef_X1Y1(1),groundRef_X1Y1(2),'o','Color','Yellow', 'LineWidth', 3);
+        plot(groundRef_X2Y2(1),groundRef_X2Y2(2),'o','Color','Cyan', 'LineWidth', 3);      
+        % Switch to refFrm handle and draw result
+        figure(curFrm_figHandle);       
+        hold on;
+        plot(realCoor_X1Y1(1),realCoor_X1Y1(2),'o','Color','Yellow', 'LineWidth', 3);
+        plot(realCoor_X2Y2(1),realCoor_X2Y2(2),'o','Color','Cyan', 'LineWidth', 3);
+    else
+        subplot(curFrm_subplotHandle(plot_index)), hold on;
+        title(['Strip[', num2str(plot_index), ']'], 'Color', 'Black');
+        % ((((This part handle the image strip indexing))))
+        plot_index = plot_index + 1;
+        plot_col = plot_col + 1;
+        % If column reach the end, go to next row
+        if (plot_col > size(curFrm_strip,2))
+            plot_row = plot_row + 1;
+            plot_col = 1;
+            % Done with plotting, exit the loop
+            if (plot_row > size(curFrm_strip,1))
+                done = true;
+            end
         end
     end
-end
+ end
 % figure(refFrm_figHandle);       
 % hold on;
 % j = 1;
