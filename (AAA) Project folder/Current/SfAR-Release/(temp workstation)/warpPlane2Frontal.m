b0df356a785f3cc08dc0f412a07a76d4a08c0dd4
Jack Cho <jack06215@gmail.com>
2016-04-05 17:59:38 +1000

    Recovery

--- (AAA) Project folder/Current/SfAR-Release/(temp workstation)/warpPlane2Frontal.m
+++ (AAA) Project folder/Current/SfAR-Release/(temp workstation)/warpPlane2Frontal.m
@@ -2,13 +2,31 @@
 ax=X3(1);ay=X3(2);az=X3(3);
 R1=makehgtform('xrotate',ax,'yrotate',ay, 'zrotate',az); 
 R1=R1(1:3,1:3);
-img_center = [1,0, -center(1); 0,1, -center(2); 0,0,1];
-H1= K*(R1/K)*img_center;
-%% Calclating resultant translation and scale
+C_center = [1,0, -center(1);
+            0,1, -center(2);
+            0,0,1];
+H1= K*(R1/K)*C_center;
+%% Calclating Resultant Translation and Scale
 Rect = [0,0,1; size(im,2),0,1; size(im,2),size(im,1),1; 0,size(im,1),1]';
+Rect_out = homoTrans(H1, Rect);
+%% Fix scaling, based on length.
+scale_fac = abs((max(Rect_out(1,2), Rect_out(1,3))- min(Rect_out(1,1), Rect_out(1,4)))/size(im,2));
+Rect_out = Rect_out./repmat(scale_fac,3,4);
+%% Shift the Rect_out back to "pixel coordinate" w.r.t. Rect
+Rect_out(1,2) = Rect_out(1,2) - Rect_out(1,1);
+Rect_out(2,2) = Rect_out(2,2) - Rect_out(2,1);
+Rect_out(1,3) = Rect_out(1,3) - Rect_out(1,1);
+Rect_out(2,3) = Rect_out(2,3) - Rect_out(2,1);
+Rect_out(1,4) = Rect_out(1,4) - Rect_out(1,1);
+Rect_out(2,4) = Rect_out(2,4) - Rect_out(2,1);
 Rect_out(1,1) = Rect_out(1,1) - Rect_out(1,1);
-Rect_prespective = homoTrans(H1, Rect);
-%% Fix scaling, based on width
-scale_fac = abs((max(Rect_prespective(1,2), Rect_prespective(1,3))- min(Rect_prespective(1,1), Rect_prespective(1,4)))/size(im,2));
-Rect_prespective = Rect_prespective./repmat(scale_fac,3,4);
-%% Shift the Rect_out baconfig.swiftt.com
+Rect_out(2,1) = Rect_out(2,1) - Rect_out(2,1);
+% Conversion: dropping off the 'w coordinate', and transpose
+Rect = Rect(1:2,:)'; 
+Rect_out = Rect_out(1:2,:)';
+%% Fit geometric transform between Rect and Rect_out
+T = fitgeotrans(Rect,Rect_out,'projective');
+im_new = imwarp(im, T');
+hFig=[hFig az_fig];
+set(hFig(1,end),'Name','Gap Filled and Extended Lines');
+imagesc(im_new), axis equal;
