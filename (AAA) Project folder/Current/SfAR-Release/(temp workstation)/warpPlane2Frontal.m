f9c5fcdba870d6cab76e9011a84e117389e2f454
Jack Cho <jack06215@gmail.com>
2016-04-05 17:16:53 +1000

    Code cleaning for "Frontal view warping"

--- (AAA) Project folder/Current/SfAR-Release/(temp workstation)/warpPlane2Frontal.m
+++ (AAA) Project folder/Current/SfAR-Release/(temp workstation)/warpPlane2Frontal.m
@@ -6,10 +6,10 @@ C_center = [1,0, -center(1);
             0,1, -center(2);
             0,0,1];
 H1= K*(R1/K)*C_center;
-%% Calclating Resultant Translation and Scale
+%% Calclating resultant translation and scale
 Rect = [0,0,1; size(im,2),0,1; size(im,2),size(im,1),1; 0,size(im,1),1]';
 Rect_out = homoTrans(H1, Rect);
-%% Fix scaling, based on length.
+%% Fix scaling, based on length
 scale_fac = abs((max(Rect_out(1,2), Rect_out(1,3))- min(Rect_out(1,1), Rect_out(1,4)))/size(im,2));
 Rect_out = Rect_out./repmat(scale_fac,3,4);
 %% Shift the Rect_out back to "pixel coordinate" w.r.t. Rect
