906723b7d957c0b79aa9b532605a0d4d052e9afd
Jack Cho <jack06215@gmail.com>
2016-04-14 15:00:11 +1000

    Experiment, no change to code is made

---
+++ (AAA) Project folder/Current/SfAR-Release/(temp workstation)/simpleHomoPlaneWarping.m
@@ -0,0 +1,17 @@
+%% Define principle center
+%       1   0   CenterX
+% T =   0   1   CenterY
+%       0   0     1
+T_center = [1,0,center(1); 0,1,center(2);0,0,1];
+
+%% Construct rotation matrix
+ax=X3(1);ay=X3(2);az=X3(3);
+R1=makehgtform('xrotate',ax,'yrotate',ay, 'zrotate',az);
+R1=R1(1:3,1:3);
+%% Construct homography induced by plane
+H2 = K * R1/K;
+H2 = (T_center * H2/T_center);
+%% Make tfrom and wapr image
+tform = projective2d(H2');
+cut_img = imwarp(im,tform);
+figure, imshow(cut_img);
